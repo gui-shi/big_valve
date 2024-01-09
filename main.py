a225a730b13b1e9db0749d3c5641163a91cae66d
Gaein nidb <mail@gaein.cn>
2024-01-10 02:02:12 +0800

    feat: 添加 websocket 以及异步

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import predict
+import server
+import asyncio
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    STREAM_ADDR = "rtsp://192.168.23.23:8554/stream0"
 
-    predict.predict_stream(STREAM_ADDR)
+async def main():
+    STREAM_ADDR = "rtmp://v4-cn-bj3.gaein.cn/live/livestream"
+
+    websocket_service = asyncio.create_task(server.start("0.0.0.0", 9331))
+    predict_service = asyncio.create_task(predict.predict_stream(STREAM_ADDR))
+
+    try:
+        await asyncio.gather(websocket_service, predict_service)
+    except KeyboardInterrupt:
+        print("Cancelled by user.")
+        websocket_service.cancel()
+        #    predict_service.cancel()
+        #  await asyncio.gather(websocket_service, predict_service, return_exceptions=True)
+
+
+asyncio.run(main())
---
+++ server.py
@@ -0,0 +1,84 @@
+import asyncio
+import math
+import cv2
+import numpy
+import websockets
+import websockets.exceptions
+from bitstring import BitArray
+from loguru import logger
+
+# 存储所有连接的客户端
+sockets = set()
+
+
+async def start(host: str, port: int):
+    logger.success(f"WS server listen on {host}, port {port}")
+    async with websockets.serve(server, host, port):
+        await asyncio.Future()
+
+
+# 启动WebSocket服务器
+async def server(websocket):
+    # 添加新连接的客户端到集合中
+    logger.info(f"add websocket client {websocket}")
+    sockets.add(websocket)
+    try:
+        # 循环监听客户端消息
+        async for message in websocket:
+            # 在服务器端打印接收到的消息
+            logger.info(f"Received message from {websocket}: {message}")
+            await websocket.send("pong")
+            # 如果有需要，可以在这里处理接收到的消息
+
+    except websockets.exceptions.ConnectionClosedError:
+        # 处理连接关闭时的异常
+        pass
+    finally:
+        # 从集合中移除已关闭连接的客户端
+        logger.info(f"remove websocket client {websocket}.")
+        sockets.remove(websocket)
+
+
+async def send_predict_result(percent: float, origin_img: numpy.array, masked_img: numpy.array):
+    percent_in_int = math.floor(percent * 1000 + 0.5)
+
+    """
+    | 百分比     | 原图长度                      | 掩码图长度                 | 原图二进制 | 掩码图二进制 |
+    | percent   | Content-Length of origin    | Content-Length of masked  | origin    | masked    |
+    | 16bit     | 24bit                         | 24bit                     | val     | val        |
+    """
+
+    bit_of_percent = 16
+    bit_of_content_length = 24
+
+    _, origin_img_bin = cv2.imencode(".webp", origin_img)
+    _, masked_img_bin = cv2.imencode(".webp", masked_img)
+
+    origin_img_bin = origin_img_bin.tobytes()
+    masked_img_bin = masked_img_bin.tobytes()
+
+    origin_img_len = len(origin_img_bin)
+    masked_img_len = len(masked_img_bin)
+
+    if origin_img_len == 0:
+        logger.error("origin image size 0")
+        return
+    if masked_img_len == 0:
+        logger.error("masked image size 0")
+
+    metadata_bin = BitArray(
+        (f'int:{bit_of_percent}={percent_in_int}, '
+         f'int:{bit_of_content_length}={origin_img_len}, '
+         f'int:{bit_of_content_length}={masked_img_len}'))
+    packet_bin = (metadata_bin + BitArray(bytes=origin_img_bin) + BitArray(bytes=masked_img_bin)).tobytes()
+
+    logger.info(f"packet data, len:{len(packet_bin)}")
+
+    # 遍历所有连接的客户端，发送消息
+    logger.info(f"send message to {len(sockets)} clients.")
+    for client in sockets:
+        try:
+            await client.send(packet_bin)
+        except websockets.exceptions.ConnectionClosedError:
+            # 处理连接已关闭的异常
+            pass
