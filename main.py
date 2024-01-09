7e381845ccda9a156e74e8bc59635426111aa506
Gaein nidb <mail@gaein.cn>
2024-01-10 03:46:58 +0800

    fix: 异步只能拿到第一帧，先放弃了

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 
 
 async def main():
-    STREAM_ADDR = "rtmp://v4-cn-bj3.gaein.cn/live/livestream"
+    STREAM_ADDR = "rtsp://v4-cn-bj3.gaein.cn:8554/live/livestream"
 
     websocket_service = asyncio.create_task(server.start("0.0.0.0", 9331))
     predict_service = asyncio.create_task(predict.predict_stream(STREAM_ADDR))
--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+import asyncio
+import hashlib
 import os
 import cv2
 import ffmpeg
@@ -5,62 +7,81 @@ import numpy as np
 from loguru import logger
 from ultralytics import YOLO
 
+import server
+
 model_path = os.path.split(os.path.realpath(__file__))
 
 model = YOLO(os.path.join('models/best.pt'))
 
 ORIGIN_WIDTH, ORIGIN_HIGH = 1920, 1080
-CROP_WIDTH, CROP_HIGH = 960, 960
+CROP_WIDTH, CROP_HIGH = 1080, 1080
 TARGET_WIDTH, TARGET_HIGH = 640, 640
 
 
-def predict_stream(url: str, debug=False):
+async def predict_stream(url: str, debug=False):
     frame_seq = 0
+
+    # process = (ffmpeg
+    #            .input(filename=url, rtsp_transport='tcp', re=None)
+    #            .filter('select', 'eq(pict_type,I)')
+    #            .filter('scale', ORIGIN_WIDTH, ORIGIN_HIGH)
+    #            .filter('crop', CROP_WIDTH, CROP_HIGH, (ORIGIN_WIDTH - CROP_WIDTH) / 2,
+    #                    (ORIGIN_HIGH - CROP_HIGH) / 2)
+    #            .filter('scale', TARGET_WIDTH, TARGET_HIGH)
+    #            .output('pipe:', format='rawvideo', pix_fmt='bgr24')
+    #            .run_async(pipe_stdout=True, pipe_stderr=True))
 
     while True:
         frame_seq += 1
 
+        # frame, err = await asyncio.to_thread(process.communicate)
+        # frame = process.stdout.read(TARGET_WIDTH * TARGET_HIGH * 3)
+
+        # err = process.stderr.readline()
+        # if err:
+        # logger.error(err)
+        # process.stdout.seek(TARGET_WIDTH * TARGET_HIGH * 3)
+
         frame, err = ffmpeg \
-            .input(filename=url, skip_frame='nokey', re=None) \
+            .input(filename=url, re=None) \
             .filter('select', 'eq(pict_type,I)') \
             .filter('scale', ORIGIN_WIDTH, ORIGIN_HIGH) \
             .filter('crop', CROP_WIDTH, CROP_HIGH, (ORIGIN_WIDTH - CROP_WIDTH) / 2,
                     (ORIGIN_HIGH - CROP_HIGH) / 2) \
             .filter('scale', TARGET_WIDTH, TARGET_HIGH) \
-            .output('pipe:', vframes=1, format='rawvideo', pix_fmt='bgr24', loglevel="quiet") \
+            .output('pipe:', vframes=1, format='rawvideo', pix_fmt='bgr24', loglevel='error') \
             .run(capture_stdout=True)
+
         if not frame:
             logger.error("Not frame!")
             continue
 
-        # logger.info("Get frame here!")
+        logger.info(hashlib.md5(frame).hexdigest())
+
         np_image = np.frombuffer(frame, np.uint8).reshape([TARGET_HIGH, TARGET_WIDTH, 3])
 
-        #  cv2.imshow("show", np_image)
-        #  cv2.waitKey()
-        percent = predict.predict_np_image(np_image, frame_seq, debug=True)
+        # cv2.imshow("ffmpeg output frame", np_image)
+        # cv2.waitKey()
+
+        percent = await predict_np_image(np_image, frame_seq, debug=debug)
 
         logger.info(f"frame:{frame_seq}, percent:{percent}")
-
-        if percent is None:
-            continue
-        if percent < 0.55:
-            logger.info("阀门全开")
-        elif percent > 0.85:
-            logger.info("阀门全闭")
-        else:
-            logger.info("阀门半开")
+        await asyncio.sleep(1)
 
 
-def predict_np_image(image: np.array, frame_id=-1, debug=False) -> float or None:
-    results = model.predict(image, conf=0.6)
+async def predict_np_image(image: np.array, frame_id=-1, debug=False) -> float or None:
+    results = model.predict(image, conf=0.6, stream=True)
 
-    if len(results) == 0:
+    result_list = []
+    for result in results:
+        result_list.append(result)
+
+    if len(result_list) == 0:
         logger.warning("No valve detected.")
-    elif len(results) != 1:
-        logger.warning(f"Detected mult({len(results)}) result.")
+    elif len(result_list) != 1:
+        logger.warning(f"Detected mult({len(result_list)}) result.")
     else:
-        best_result = sorted(results, key=lambda result: result.boxes.conf, reverse=True)[0]
+        best_result = sorted(result_list, key=lambda sig_result: sig_result.boxes.conf, reverse=True)[0]
 
         masks = best_result.masks  # 分割掩码输出的 Masks 对象
 
@@ -68,15 +89,12 @@ def predict_np_image(image: np.array, frame_id=-1, debug=False) -> float or None
             logger.warning("No valve mask in result.")
             return None
 
-        all_mask = masks.data.cpu().numpy()
-
-        valve_mask = all_mask[0].astype(np.uint8)
+        valve_mask = masks.data.cpu().numpy()[0].astype(np.uint8)
 
         mask_pixel = np.count_nonzero(valve_mask)
-        # cv2.imwrite("result.bmp", valve_mask)
 
+        # 计算轮廓
         contours, _ = cv2.findContours(valve_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
         if len(contours) < 0:
             logger.error("No counters found.")
         else:
@@ -84,18 +102,23 @@ def predict_np_image(image: np.array, frame_id=-1, debug=False) -> float or None
 
             largest_contour = max(contours, key=cv2.contourArea)
             largest_contour = largest_contour.squeeze()
-            # 拟合外接圆
+            # 根据轮廓计算外接圆
             (x, y), radius = cv2.minEnclosingCircle(largest_contour)
-
+            valve_mask_result = valve_mask.copy() * 255
+            # 在图形上绘制外接圆
+            cv2.circle(valve_mask_result, (int(x), int(y)), int(radius), 255, 2)
             if debug:
-                result_image = valve_mask.copy() * 255
-                cv2.circle(result_image, (int(x), int(y)), int(radius), 128, 2)
-                cv2.imwrite(f"test/{frame_id}_test_result.bmp", result_image)
-                cv2.imwrite(f"test/{frame_id}_test.bmp", image)
+                cv2.imwrite(f"test/{frame_id}_result.bmp", valve_mask_result)
+                cv2.imwrite(f"test/{frame_id}.bmp", image)
             # 计算外接圆的面积
             circle_area = np.pi * (radius ** 2)
             percent = mask_pixel / circle_area
 
-            # logger.info(f"阀门占比是{percent}")
+            # cv2.imshow("test", best_result.plot())
+            # cv2.waitKey()
+
+            await server.send_predict_result(percent, best_result.orig_img, valve_mask_result)
+
+            logger.success(f"Open percent is: {percent:.3%}")
 
             return percent
